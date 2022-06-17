4584a9b94c7fe311348ec460edb549e22ab838ad
David Baldassin <david.baldassin@cosmosoftware.io>
2022-06-17 14:52:35 +0200

    add graph send/recv kbps

--- www/js/index.js
+++ www/js/index.js
@@ -1,4 +1,24 @@
 
+// Request ids
+const START_REQUEST = 0;
+const STOP_REQUEST = 1;
+const LINK_REQUEST = 2;
+const OUT_REQUEST = 3;
+
+// time
+const SEC = 1000;
+const MIN = 60 * SEC;
+
+// LINK
+const LINK_LIMIT = [[60, 1000, 1], [30, 500, 1], [30, 1000, 1]]; // [time to wait, link cap, delay]
+
+let stats = [];
+let stats_send = [];
+let link_chart = [];
+let current_link = 0;
+
+let sessionId = 0;
+
 function getLocalVideo() {
     return document.getElementById('local');
 }
@@ -26,7 +46,7 @@ var opts = {
 
 var targets = document.querySelectorAll('.gaugeChart'); // your canvas element
 var gauges = [];
-for (var i=0;i<targets.length;++i)
+for (var i=0; i < targets.length; ++i)
 {
 	gauges[i] = new Gauge(targets[i]).setOptions(opts); // create sexy gauge!
 	gauges[i].animationSpeed = 10000; // set animation speed (32 is default value)
@@ -37,6 +57,7 @@ gauges[0].maxValue = 1280;
 gauges[1].maxValue = 720; 
 gauges[2].maxValue = 30; 
 gauges[3].maxValue = 1024; 
+gauges[4].maxValue = 1024; 
 
 var texts =  document.querySelectorAll('.gaugeChartLabel');
 
@@ -71,7 +92,7 @@ async function createLocalPeerConnection(configuration) {
     const offer = await pc.createOffer([{ offerToReceiveVideo: true, offerToReceiveAudio: false }]);
     await pc.setLocalDescription(offer);
 
-    console.log({offer: offer.sdp});
+    console.log({ offer: offer.sdp });
     
     return { pc, offer };
 }
@@ -80,11 +101,99 @@ let playing = true;
 let local_pc = null;
 let remote_pc = null;
 
+var prev = 0, prevFrames = 0, prevBytes = 0;
+var prev1 = 0, prevFrames1 = 0, prevBytes1 = 0;
+
+async function get_remote_stats(track, count) {
+    var results;
+    let remote = getRemoteVideo();
+
+    try {
+	//For ff
+	results = await remote_pc.pc.getStats(track);
+    } catch(e) {
+	//For chrome
+	results = await remote_pc.pc.getStats();
+    }
+    //Get results
+    for (let result of results.values())
+    {
+	if (result.type==="inbound-rtp")
+	{
+	    //Get timestamp delta
+	    var delta = result.timestamp-prev;
+	    //Store this ts
+	    prev = result.timestamp;
+
+	    //Get values
+	    var width = track.width || remote.videoWidth;//result.stat("googFrameWidthReceived");
+	    var height = track.height || remote.videoHeight;//result.stat("googFrameHeightReceived");
+	    var fps =  (result.framesDecoded-prevFrames)*1000/delta;
+	    var kbps = (result.bytesReceived-prevBytes)*8/delta;
+	    //Store last values
+	    prevFrames = result.framesDecoded;
+	    prevBytes  = result.bytesReceived;
+	    //If first
+	    if (delta==result.timestamp || isNaN(fps) || isNaN (kbps))
+		return;
+
+	    for (var i=0;i<targets.length;++i)
+		gauges[i].animationSpeed = 10000000; // set animation speed (32 is default value)
+	    gauges[0].set(width);
+	    gauges[1].set(height);
+	    gauges[2].set(Math.min(Math.floor(fps)   ,30));
+	    gauges[3].set(Math.min(Math.floor(kbps) ,1024));
+	    texts[0].innerText = width;
+	    texts[1].innerText = height;
+	    texts[2].innerText = Math.floor(fps);
+	    texts[3].innerText =  Math.floor(kbps);
+
+	    stats.push({x: count, y: Math.floor(kbps)});
+	    link_chart.push({x: count, y: LINK_LIMIT[current_link][1]});
+	    ++count;
+	}
+    }
+}
+
+async function get_local_stats(track, count) {
+    var results;
+    let local = getLocalVideo();
+
+    try {
+	//For ff
+	results = await local_pc.pc.getStats(track);
+    } catch(e) {
+	//For chrome
+	results = await local_pc.pc.getStats();
+    }
+    //Get results
+    for (let result of results.values())
+    {
+	if (result.type === "outbound-rtp")
+	{
+	    //Get timestamp delta
+	    var delta = result.timestamp - prev1;
+	    //Store this ts
+	    prev1 = result.timestamp;
+	    
+	    var kbps = (result.bytesSent - prevBytes1) * 8 / delta;
+	    //Store last values
+	    prevBytes1 = result.bytesSent;
+	    //If first	    
+	    if (delta == result.timestamp || isNaN(kbps)) return;
+
+	    gauges[4].set(Math.min(Math.floor(kbps) ,1024));
+	    texts[4].innerText =  Math.floor(kbps);
+
+	    stats_send.push({x: count, y: Math.floor(kbps)});
+	}
+    }
+}
+
 async function createRemotePeerConnection(configuration, offer) {
     const pc = new RTCPeerConnection(configuration);
     
     pc.addEventListener('track', async function(e) {
-	var prev = 0,prevFrames = 0,prevBytes = 0;
 	console.log("on track");
 	const [remoteStream] = e.streams;
 	let remote = getRemoteVideo();
@@ -92,61 +201,28 @@ async function createRemotePeerConnection(configuration, offer) {
 	remote.srcObject = remoteStream;
 	remote.play();
 
-	let track = remoteStream.getVideoTracks()[0];
-	let interval = setInterval(async function(){
-	    var results;
+	let remotetrack = remoteStream.getVideoTracks()[0];
 
-	    try {
-		//For ff
-		results = await remote_pc.pc.getStats(track);
-	    } catch(e) {
-		//For chrome
-		results = await remote_pc.pc.getStats();
-	    }
-	    //Get results
-	    for (let result of results.values())
-	    {
-		if (result.type==="inbound-rtp")
-		{
-		    //Get timestamp delta
-		    var delta = result.timestamp-prev;
-		    //Store this ts
-		    prev = result.timestamp;
+	let count = 0;
+	
+	let interval = setInterval(async function() {
+	    get_remote_stats(remotetrack, count);
 
-		    //Get values
-		    var width = track.width || remote.videoWidth;//result.stat("googFrameWidthReceived");
-		    var height = track.height || remote.videoHeight;//result.stat("googFrameHeightReceived");
-		    var fps =  (result.framesDecoded-prevFrames)*1000/delta;
-		    var kbps = (result.bytesReceived-prevBytes)*8/delta;
-		    //Store last values
-		    prevFrames = result.framesDecoded;
-		    prevBytes  = result.bytesReceived;
-		    //If first
-		    if (delta==result.timestamp || isNaN(fps) || isNaN (kbps))
-			return;
-
-		    for (var i=0;i<targets.length;++i)
-			gauges[i].animationSpeed = 10000000; // set animation speed (32 is default value)
-		    gauges[0].set(width);
-		    gauges[1].set(height);
-		    gauges[2].set(Math.min(Math.floor(fps)   ,30));
-		    gauges[3].set(Math.min(Math.floor(kbps) ,1024));
-		    texts[0].innerText = width;
-		    texts[1].innerText = height;
-		    texts[2].innerText = Math.floor(fps);
-		    texts[3].innerText =  Math.floor(kbps);
-		}
-	    }
+	    let local = getLocalVideo();
+	    let localtrack = local.streamObject.getVideoTracks()[0];
+	    get_local_stats(localtrack, count);
+	    
+	    count++;
 	}, 1000);
 
-	track.addEventListener("ended", (event) =>{
-		console.debug("onended", event);
-		clearInterval(interval);
+	remotetrack.addEventListener("ended", (event) =>{
+	    console.debug("onended", event);
+	    clearInterval(interval);
 	});
     });
 
     pc.addEventListener('connectionstatechange', event => {
-	console.log("remote pc : " + pc.connectionState);
+	console.log("remote pc : " + pc.connectionState);	
     });
 
     await pc.setRemoteDescription(new RTCSessionDescription(offer));    
@@ -201,7 +277,7 @@ async function start_peerconnection(port) {
 	    console.log("add local", event.candidate);
             local_pc.pc.addIceCandidate(event.candidate);
 	}
-    });    
+    });
 }
 
 function stop_peerconnection() {
@@ -218,10 +294,143 @@ function stop_peerconnection() {
     remote_pc.pc.close();
 }
 
+function start_peerconnection_medooze(port) {
+    let ws = new WebSocket("wss://localhost:8084", "quic-relay");
+    let local = getLocalVideo();
+    local.play();
+
+    ws.onopen = async () => {
+	local_pc = new RTCPeerConnection();
+	const stream = local.captureStream();
+	local.streamObject = stream;
+
+	local_pc.addEventListener('connectionstatechange', event => {
+	    console.log("local pc : " + local_pc.connectionState);
+	});
+    
+	stream.getVideoTracks().forEach(t => local_pc.addTrack(t, stream));
+
+	const offer = await local_pc.createOffer();
+	await local_pc.setLocalDescription(offer);
+
+	ws.send(JSON.stringify({ cmd: "publish", offer: offer.sdp }));
+    };
+    
+    ws.onclose = async () => {
+	local_pc.close();
+    };
+
+    ws.onmessage = async (msg) => {
+	console.log(msg);
+	let ans = JSON.parse(msg.data);
+	if(ans.answer) {
+	    local_pc.setRemoteDescription(new RTCSessionDescription({
+		type:'answer',
+		sdp: ans.answer
+	    }));
+	}
+    };
+}
+
+function reset_link(ws) {
+    let linkObject = {
+	cmd: "link",
+	transId: LINK_REQUEST,
+	data: {}
+    };
+    
+    ws.send(JSON.stringify(linkObject));
+
+    let bitrate = document.getElementById('bitrate');
+    let delay = document.getElementById('delay');
+
+    bitrate.value = "";
+    delay.value = "";
+}
+
+function set_link_interval(ws, current) {
+    if(current >= LINK_LIMIT.length) {
+	reset_link(ws);
+	stop(ws);
+    } else {
+	current_link = current;
+	let link = LINK_LIMIT[current];
+	let time = link[0];
+	let bitrate = link[1];
+	let delay = link[2];
+	
+	let linkObject = {
+	    cmd: "link",
+	    transId: LINK_REQUEST,
+	    data: {
+		bitrate: bitrate,
+		delay: delay
+	    }
+	};
+
+	ws.send(JSON.stringify(linkObject));
+
+	setTimeout(set_link_interval, time * SEC, ws, current + 1);
+    }
+}
+
+function stop(ws) {
+    let callButton = document.querySelector('button');
+    
+    callButton.innerHTML = "Start";
+    stop_peerconnection();
+    let request = {
+        cmd: "stopclient",
+        transId: 1,
+        data: {
+            id: sessionId
+        }
+    };
+    
+    ws.send(JSON.stringify(request));
+
+    $.get("https://canvasjs.com/services/data/datapoints.php?xstart=5&ystart=10&length=10&type=csv", function(data) {
+	var chart = new CanvasJS.Chart("chartContainer", {
+            title: {
+		text: "Chart from CSV",
+            },
+            data: [{
+		type: "line",
+		dataPoints: stats
+	    }, {
+		type: 'line',
+		dataPoints: link_chart
+	    }, {
+		type: 'line',
+		dataPoints: stats_send
+	    }]
+	});
+	
+	chart.render();
+    });
+}
+/*
+$.get("https://canvasjs.com/services/data/datapoints.php?xstart=5&ystart=10&length=10&type=csv", function(data) {
+	var chart = new CanvasJS.Chart("chartContainer", {
+            title: {
+		text: "Chart from CSV",
+            },
+            data: [{
+		type: "line",
+		dataPoints: [{x: 0, y: 1}, {x: 1, y: 1}, {x: 2, y: 1}] 
+	    }, {
+		type: "line",
+		dataPoints: [{x: 0, y: 3}, {x: 1, y: 4}, {x: 2, y: 5}] 
+	    }]
+	});
+	
+	chart.render();
+    });
+*/
 (function() {
     let ws = new WebSocket("ws://dabaldassi.fr:3333");
     // let ws = new WebSocket("ws://localhost:3333");
-    let sessionId = 0;
+    let medooze = false;
 
     ws.onopen = () => {	console.log("websocket is opened"); };
     ws.onerror = () => { console.log("error with websocket"); };
@@ -233,11 +442,15 @@ function stop_peerconnection() {
 	    console.error("Error from server :" + response.data.message);
 	}
 	else if(response.type === "response") {
-	    if(response.transId === 0) {
+	    if(response.transId === START_REQUEST) {
 		sessionId = response.data.id;
-		start_peerconnection(response.data.port);
+		if(medooze) start_peerconnection_medooze(response.data.port);
+		else {
+		    start_peerconnection(response.data.port);
+		    set_link_interval(ws, 0);
+		}
 	    }
-	    else {
+	    else if(response.transId === STOP_REQUEST) {
 		window.open(response.data.url, '_blank').focus();
 	    }
 	}
@@ -262,37 +475,26 @@ function stop_peerconnection() {
 
 	    let dgram_radio = document.getElementsByName("datagrams");
 	    let dgram;
-	    
-	    dgram_radio.forEach(function(e) {
-		if(e.checked) dgram = e;
-	    });
+	    dgram_radio.forEach(e => dgram = e.checked && e.value === "datagram");
+
+	    let relay_radio = document.getElementsByName("relay");
+	    relay_radio.forEach(e => medooze = e.checked && e.value === "medooze");
 	    
 	    let request = {
 		cmd: "startclient",
-		transId: 0,
+		transId: START_REQUEST,
 		data: {
-		    datagrams: dgram.value === "datagram",
+		    datagrams: dgram,
 		    cc: cc
 		}
 	    };
 
-	    console.log(request);
+	    // console.log(request);
+
+	    // if(medooze) start_peerconnection_medooze(9000);
 	    
 	    ws.send(JSON.stringify(request));
-	} else {
-	    callButton.innerHTML = "Start";
-	    stop_peerconnection();
-
-	    let request = {
-		cmd: "stopclient",
-		transId: 1,
-		data: {
-		    id: sessionId
-		}
-	    };
-
-	    ws.send(JSON.stringify(request));	    
-	}
+	} else stop(ws);
     };
 
     linkButton.onclick = function(_) {
@@ -301,7 +503,7 @@ function stop_peerconnection() {
 	
 	let linkObject = {
 	    cmd: "link",
-	    transId: 2,
+	    transId: LINK_REQUEST,
 	    data: {
 		bitrate: parseInt(bitrate.value, 10),
 		delay: parseInt(delay.value, 10)
@@ -311,19 +513,5 @@ function stop_peerconnection() {
 	ws.send(JSON.stringify(linkObject));
     };
     
-    resetLinkButton.onclick = function(_) {
-	let linkObject = {
-	    cmd: "link",
-	    transId: 2,
-	    data: {}
-	};
-	
-	ws.send(JSON.stringify(linkObject));
-
-	let bitrate = document.getElementById('bitrate');
-	let delay = document.getElementById('delay');
-
-	bitrate.value = "";
-	delay.value = "";
-    };
+    resetLinkButton.onclick = (_) => reset_link(ws);
 })();
